df9d342c91ebbd8a9a207fbfb4516f1a7afd7353
SigvardsK <59480272+SigvardsK@users.noreply.github.com>
2022-02-03 06:42:36 +0200

    first commit
    index.html set up
    basic css set up

---
+++ app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+
+        },
+        {
+            name: 'burger',
+            img: 'images/burger.png'
+
+        },
+        {
+            name: 'burger',
+            img: 'images/burger.png'
+
+        },
+        {
+            name: 'coke',
+            img: 'images/coke.png'
+
+        },
+        {
+            name: 'coke',
+            img: 'images/coke.png'
+
+        },
+    ]
+
+    // create game board
+    
+
+})
