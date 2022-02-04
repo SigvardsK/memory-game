521f34bcef633a12070bd8102e7709a55efc3dce
SigvardsK <59480272+SigvardsK@users.noreply.github.com>
2022-02-04 13:47:03 +0200

    commit
    added images + basic game function

--- app.js
+++ app.js
@@ -3,38 +3,86 @@ document.addEventListener('DOMContentLoaded', () => {
     // card options
     const cardArray = [
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'Danilans',
+            img: 'images/Danilans.png'
 
         },
         {
-            name: 'fries',
-            img: 'images/fries.png'
+            name: 'Danilans',
+            img: 'images/Danilans.png'
 
         },
         {
-            name: 'burger',
-            img: 'images/burger.png'
+            name: 'tofiks',
+            img: 'images/tofiks.png'
 
         },
         {
-            name: 'burger',
-            img: 'images/burger.png'
+            name: 'tofiks',
+            img: 'images/tofiks.png'
 
         },
         {
-            name: 'coke',
-            img: 'images/coke.png'
+            name: 'vaira',
+            img: 'images/vaira.png'
 
         },
         {
-            name: 'coke',
-            img: 'images/coke.png'
+            name: 'vaira',
+            img: 'images/vaira.png'
+
+        },
+        {
+            name: 'kakis',
+            img: 'images/kakis.png'
+
+        },
+        {
+            name: 'kakis',
+            img: 'images/kakis.png'
+
+        },
+        {
+            name: 'saule',
+            img: 'images/saule.png'
+
+        },
+        {
+            name: 'saule',
+            img: 'images/saule.png'
+
+        },
+        {
+            name: 'laima',
+            img: 'images/laima.png'
+
+        },
+        {
+            name: 'laima',
+            img: 'images/laima.png'
 
         },
     ]
 
+    const grid = document.querySelector('.grid')
+
     // create game board
-    
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blue.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    // check for matches
+
+
+    // flip card
+
+    createBoard()
+
 
 })
