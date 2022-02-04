550ea77559495af4d4ace51109ffa7bbb6abf233
SigvardsK <59480272+SigvardsK@users.noreply.github.com>
2022-02-04 21:13:43 +0200

    Update app.js
    created working game v.0.1.

--- app.js
+++ app.js
@@ -64,7 +64,14 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     ]
 
+    cardArray.sort (() => 0.5 - Math.random())
+
     const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = [] 
+
 
     // create game board
     function createBoard() {
@@ -72,17 +79,46 @@ document.addEventListener('DOMContentLoaded', () => {
             var card = document.createElement('img')
             card.setAttribute('src', 'images/blue.png')
             card.setAttribute('data-id', i)
-            // card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipcard)
             grid.appendChild(card)
         }
     }
 
     // check for matches
-
+    function checkForMatch () {
+        let cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTWoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTWoId].setAttribute('src', 'īmages/white.png')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blue.png')
+            cards[optionTWoId].setAttribute('src', 'images/blue.png')
+            alert('Sorry, try again!')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray/2) {
+            resultDisplay.textContent = 'Congratulations! You won!'
+        }
+    }
 
     // flip card
+    function flipcard () {
+        let cardId = this.getAttribute ('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src',cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    } 
 
-    createBoard()
+     createBoard()
 
 
 })
